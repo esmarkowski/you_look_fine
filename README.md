a166099ff1679b432f5c113077888ee583cb234f
Spencer Markowski <spencer@theablefew.com>
2023-11-11 04:49:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# OpenAI Assistant
+
+This Python script, detect_motion_mac.py, is designed to detect motion through a webcam. When motion is detected, it sends a request to an AI assistant, which generates a compliment based on the image captured. The compliment is then converted to speech and played back.
+
+The script accepts several command-line arguments:
+
+--debug: Runs the program in debug mode. In this mode, instead of sending a request to the AI assistant, the program prints a debug message.
+--run-once: Makes the program exit after a single request has been made.
+The script uses OpenCV for motion detection and the OpenAI API for generating compliments. It's designed to be run on a Mac, using the afplay command to play audio.
+
+The script handles keyboard interrupts, so you can stop it by pressing Ctrl+C. It also ensures that the webcam is properly released and all windows are destroyed when the script ends, regardless of whether it was interrupted or finished normally.
+
+
+Use at your own risk. Read 
+
+
+```
+python detect_motion.py --debug --run-once
+```
+
+## Installation
+
+1. Clone this repository:
+```
+git clone https://github.com/yourusername/yourrepository.git
+```
+
+2. Navigate to the project directory:
+```
+cd yourepository
+```
+
+3. Install the required Python packages:
+```
+pip install -r requirements.txt
+```
+
+4. Set your OpenAI API key as an environment variable:
+```
+export OPENAI_API_KEY=your_actual_key_here
+```
+
+
+## Usage
+
+Run the script from the command line with the following arguments:
+
+```
+python detect_motion.py --repeat-delay 5000 --motion-threshold 10000 --max-time-between-requests 15 --debug
+```
+
+Replace `YOUR_ASSISTANT_ID` with your actual assistant ID.
+
+## Command-line arguments
+
+- `--repeat-delay`: The delay between repetitions, in milliseconds. Default is 5000.
+- `--motion-threshold`: The motion detection threshold. Default is 10000.
+- `--max-time-between-requests`: The maximum time between requests, in seconds. Default is 15.
+- `--debug`: Enable debug mode. If this flag is present, the program will print a debug message instead of sending a request to the API.
+- `--mute`: Mutes TTS
+- `--run-once`: Runs until motion is detected and compliment is delivered. 
+- `--image-prompt TEXT`: Instructions for vision. Default: Give a short compliment based on the person's appearance in the image. Call out distinct features.  
+
+## TODO
+
+- Prevent sending new requests if another request to get a compliment has started.
+
+Please replace YOUR_OPENAI_API_KEY with your actual API key, assistant_id with the correct ID of your assistant, and text-to-speech-model with the appropriate text-to-speech model identifier. You should also handle the response from the API carefully, checking for errors and ensuring that the response.content contains the expected audio data.
+
+Note that the motion_threshold value determines the sensitivity of the motion detection and may need to be adjusted based on the specific environment and camera setup. Also, you may want to add additional logic to handle different types of responses from the API, such as errors or unexpected content.
+
+[IMAGE]
+
+"Your leather apron really speaks to the skilled artisan in you. It's not only practical but gives off a vibe of true craftsmanship. And the way you've matched it with sturdy boots and protective gloves shows you're ready for any task. Keep up the excellent work!"
+
+# Running
+
+```
+python detect_motion.py --assistant-id YOUR_ASSISTANT_ID --repeat-delay 5000 --motion-threshold 10000 --max-time-between-requests 15
+```
+
+#### Mac
+python detect_motion_mac.py --repeat-delay 5000 --motion-threshold 10000 --max-time-between-requests 500000 --debug --mute
+
+## TODO
+- prevent sending new requests if another request to get a compliment has started
+- 
---
+++ detect_motion.py
@@ -0,0 +1,167 @@
+import argparse
+import cv2
+import requests
+import json
+import subprocess
+import os
+import base64
+from pathlib import Path
+from openai import OpenAI
+
+client = OpenAI()
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Function to get a compliment based on the person's appearance in the image
+def get_compliment(image_path):
+    # Upload the image as a file to OpenAI
+
+    # Getting the base64 string
+    base64_image = encode_image(image_path, image_text)
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+
+    payload = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
+        {
+            "role": "user",
+            "content": [
+            {
+                "type": "text",
+                "text": image_text
+            },
+            {
+                "type": "image_url",
+                "image_url": {
+                "url": f"data:image/jpeg;base64,{base64_image}"
+                }
+            }
+            ]
+        }
+        ],
+        "max_tokens": 300
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+    # Process the response to extract the compliment
+    compliment = response.json()['choices'][0]['message']['content'].strip()
+    return compliment
+
+# Function to convert text to speech
+def text_to_speech(text, output_file):
+    if debug:
+        print("Debug: text_to_speech called with text =", text)
+        # return
+
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="fable",
+        input=text
+    )
+
+    response.stream_to_file(output_file)
+
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Run the OpenAI Assistant.')
+parser.add_argument('--repeat-delay', type=int, default=5000, help='The delay between repetitions, in milliseconds.')
+parser.add_argument('--motion-threshold', type=int, default=10000, help='The motion detection threshold.')
+parser.add_argument('--max-time-between-requests', type=int, default=500000, help='The maximum time between requests, in seconds.')
+parser.add_argument('--debug', action='store_true', help='Enable debug mode.')
+parser.add_argument('--mute', action='store_true', help='Mute audio.')
+parser.add_argument('--run-once', action='store_true', help='Runs until detection is made and compliment is given.')
+parser.add_argument('--vision-prompt', type=str default="Give a short compliment based on the person's appearance in the image. Call out distinct features." help='Instructions for vision.')
+args = parser.parse_args()
+
+# Use the arguments in your code
+repeat_delay = args.repeat_delay
+motion_threshold = args.motion_threshold
+max_time_between_requests = args.max_time_between_requests
+debug = args.debug
+muted = args.mute
+run_once = args.run_once
+vision_prompt = args.vision_prompt
+# Get the API key from the environment
+api_key = os.getenv('OPENAI_API_KEY')
+
+# Initialize the motion detection
+camera = cv2.VideoCapture(0)  # Use the correct camera index
+motion_detector = cv2.createBackgroundSubtractorMOG2()
+
+if debug:
+    print("Debug: Beginning")
+# Initialize the request state
+frame_counter = 0
+request_in_progress = False
+camera_warmed_up = False
+
+try:
+    while True:
+        ret, frame = camera.read()
+        if not camera_warmed_up:
+            frame_counter += 1
+            if frame_counter > 20:
+                # After 20 frames, consider the camera warmed up
+                print("Camera initialized")
+                camera_warmed_up = True
+            else:
+                # Skip this frame and go to the next iteration of the loop
+                continue
+        if not ret:
+            break
+
+        # Apply the motion detector to the frame
+        motion_mask = motion_detector.apply(frame)
+        
+        # If there's enough motion and no request is in progress, proceed
+        if motion_mask.sum() > motion_threshold and not request_in_progress:
+            
+            print("Motion detected")
+            # Set the request state to in progress
+            request_in_progress = True
+
+            # Save the frame as an image
+            image_path = '/tmp/detected_motion.jpg'
+            cv2.imwrite(image_path, frame)
+            
+            # Get a compliment for the image
+            compliment = get_compliment(image_path, vision_prompt)
+            
+            if debug:
+                print("Debug: Compliment =", compliment)
+            # Convert the compliment to speech
+            audio_file = '/tmp/compliment.mp3'
+            text_to_speech(compliment, audio_file)
+            
+            # Play the audio file
+            if not muted:
+                if platform.system() == 'Darwin':
+                    subprocess.run(["afplay", audio_file])
+                else:
+                    subprocess.run(["mpg123", audio_file])
+
+            # Set the request state back to not in progress
+            request_in_progress = False
+
+            if run_once:
+                break
+
+            # Wait for a while before detecting motion again to avoid repeats
+            cv2.waitKey(repeat_delay)
+
+        # Wait for a while before the next frame
+        cv2.waitKey(max_time_between_requests)   
+    # Release the camera and close any open windows
+except KeyboardInterrupt:
+    print("Interrupted by user. Exiting...")
+finally:
+    # Release the camera and destroy all windows
+    camera.release()
+    cv2.destroyAllWindows()
